7d0aea1de17b5881f943910f2ab7673a47501048
777irug <67226582+777irug@users.noreply.github.com>
2022-09-23 18:35:03 +0530

    Add files via upload

---
+++ src/subComponents/LogoComponent.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import styled from 'styled-components'
+import { DarkTheme } from '../components/Themes'
+
+
+const Logo = styled.h1`
+display: inline-block;
+color: ${props => props.color === 'dark' ? DarkTheme.text : DarkTheme.body};
+font-family: 'Pacifico',cursive;
+
+position: fixed;
+left: 2rem;
+top: 2rem;
+z-index:3;
+`
+
+const Logo2 = styled.h1`
+  display: inline-block;
+  color: ${(props) =>
+    props.color === "dark" ? DarkTheme.text : DarkTheme.body};
+  font-family: "Pacifico", cursive;
+
+  position: fixed;
+  left: 2rem;
+  top: 2rem;
+  z-index: 3;
+`;
+
+
+const LogoComponent = (props) => {
+    return (
+      <a href="https://portfolio-guri.vercel.app/">
+        <Logo color={props.theme}>AS</Logo>
+      </a>
+    );
+}
+
+export default LogoComponent
